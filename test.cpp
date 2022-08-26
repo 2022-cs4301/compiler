081358d4eec09f46a2199446e8d5d3f513bdfca2
[Kangmin Kim] <minrifle12@gmail.com>
2022-08-26 22:07:35 +0900

    upload

--- test.cpp
+++ test.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 
 int main()
@@ -6,5 +8,9 @@ int main()
     
     std::cout << "Welcome to CS 4301!\n";
     std::cout << "This is going to be fun!\n";
+    
+    int var[10] = {9,8,7,6,5,4,3,2,1,0};
+    std::sort(var);
+    
     return false;
 }
