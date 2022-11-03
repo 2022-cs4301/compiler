c81ee1df06cb977d5caedfae47a42bf47ad4c363
Jeff Caldwell <nemo.omen@gmail.com>
2022-11-03 11:30:26 -0500

    Commit test comment

--- stage0.cpp
+++ stage0.cpp
@@ -6,41 +6,43 @@
 #include <ctime>
 #include <cstring>
 
-Compiler::Compiler (char **argv)
+// Test comment by Jeff
+
+Compiler::Compiler(char **argv)
 {
-	sourcFile.open(argv[1]);
-	listingFile.open(argv[2]);
-	objectFile.open(argv[3]);
+  sourcFile.open(argv[ 1 ]);
+  listingFile.open(argv[ 2 ]);
+  objectFile.open(argv[ 3 ]);
 }
 
 Compiler::~Compiler()
 {
-	souceFile.close();
-	listingFile.close();
-	objectFile.close();
+  souceFile.close();
+  listingFile.close();
+  objectFile.close();
 }
 
 void Compiler::createListingHeader()
 {
-	
+
 }
 void Compiler::parser()
 {
-	
+
 }
 void Compiler::createListingTrailer()
 {
-	
+
 }
-  void prog()           // stage 0, production 1
-  {
-	  
-  }
-  void progStmt()       // stage 0, production 2
-  {
-	  
-  }
-  void consts()         // stage 0, production 3
-  {
-	  
-  }
+void prog()           // stage 0, production 1
+{
+
+}
+void progStmt()       // stage 0, production 2
+{
+
+}
+void consts()         // stage 0, production 3
+{
+
+}
